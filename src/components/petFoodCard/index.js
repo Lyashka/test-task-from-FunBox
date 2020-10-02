3260fa158e2f2c0929fef4d5bd77adea0644ccf6
Lyashka <iuliana9864@gmail.com>
2020-10-02 11:16:20 +0300

    change positioning background-cat

--- src/components/petFoodCard/index.js
+++ src/components/petFoodCard/index.js
@@ -18,7 +18,7 @@ export default class PetFoodCard extends Component {
             <p className="pet-food-card__name">Нямушка</p>
             <p className="pet-food-card__stuffing">с фуа-гра</p>
             <p className="pet-food-card__portions">10 порций</p>
-            <p className="pet-food-card__present">мышь в подарок</p>
+            <p className="pet-food-card__present">мышь в подарок<br />заказчик доволен</p>
             <div className="pet-food-card__container-volume">
               <p className="pet-food-card__volume">0,5</p>
               <p className="pet-food-card__unit">кг</p>
