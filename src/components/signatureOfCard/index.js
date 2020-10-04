8a2d785331f9ec18f26b435caadcf6992b59910d
Lyashka <iuliana9864@gmail.com>
2020-10-04 13:23:22 +0300

    change the structure of the signature component for the card

--- src/components/signatureOfCard/index.js
+++ src/components/signatureOfCard/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './signatureOfCard.scss';
 
-export default function signatureOfCard(props) {
-  return (
+function SignatureWithStatus(props) {
+  return props.cardStatus === 'default' ? (
     <footer className="footer">
       <p className="footer__signature">{`Чего сидишь? Порадуй котэ, `}
-        <button className="footer__buy-button">купи</button>.
-          </p>
+        <button className="footer__buy-button"
+          onClick={props.changeCardStatus}>
+          купи
+        </button>.
+      </p>
     </footer>
+  ) : (
+    <footer className="footer">
+      <p className="footer__signature">
+        {props.description}
+      </p>
+    </footer>
+  );
+}
+
+export default function signatureOfCard(props) {
+  return (
+    props.isDisable ? (
+      <footer className="footer_disabled">
+        <p className="footer_disabled__signature">
+          Печалька, с {props.stuffing} закончился.
+        </p>
+    </footer>
+    ) :
+    <SignatureWithStatus
+    description={props.description}
+    cardStatus={props.cardStatus}
+    changeCardStatus={props.changeCardStatus}/>
   )
 }
